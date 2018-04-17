b1d36ec83f78ed63aee801950b304a860647667f
Miguelangel Fernandez Mendoza <miguelangelxfm@gmail.com>
2018-04-17 19:25:57 +0200

    Filter out lines with at least one whitespace
    Using zero or more causes many false changes to come up in git diff.

--- filters.sh
+++ filters.sh
@@ -2,4 +2,4 @@ git config --global filter.kony_projectprop.clean "(head -n 1 %f; cat %f | grep 
 git config --global filter.kony_projectProperties.clean "jq --indent 4 --sort-keys '.nativeContainerProperties.isModified=false|.nativeContainerProperties.isEnableJSContainer=true|.nativeContainerProperties.isEnableJSContainerAndroid=true|.nativeContainerProperties.isEnableJSContaineriOS=true'"
 git config --global filter.kony_forms.clean "jq --indent 4 'del(.device, .prevDevice, .platform, .prevPlatform, .shellCSS, .context.platform)'"
 git config --global filter.kony_headless_props.clean 'sed -e "s/\(.*password.*=\).*/\1/"'
-git config --global filter.rm_whiteline.clean 'sed -e "s/^[[:blank:]]*$//"'
+git config --global filter.rm_whiteline.clean 'sed -e "s/^[[:blank:]]+$//"'
