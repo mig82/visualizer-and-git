c2d7704cf1d0c2ba7f657884a74988519a793f33
Miguelángel Fernández <miguelangelxfm@gmail.com>
2018-11-05 12:53:08 +0100

    Ignore locale to render the form in canvas

--- filters.sh
+++ filters.sh
@@ -1,5 +1,5 @@
 git config --global filter.kony_projectprop.clean "(head -n 1 %f; cat %f | grep attributes | sed /lastmodifiedtime/d | sort --ignore-case; tail -n -1 %f; echo;)"
 git config --global filter.kony_projectProperties.clean "jq --indent 4 --sort-keys '.nativeContainerProperties.isModified=false|.nativeContainerProperties.isEnableJSContainer=true|.nativeContainerProperties.isEnableJSContainerAndroid=true|.nativeContainerProperties.isEnableJSContaineriOS=true'"
-git config --global filter.kony_forms.clean "jq --indent 4 'del(.device, .prevDevice, .platform, .prevPlatform, .shellCSS, .context.platform)'"
+git config --global filter.kony_forms.clean "jq --indent 4 'del(.device, .prevDevice, .platform, .prevPlatform, .shellCSS, .context.platform, currentLocale)'"
 git config --global filter.kony_headless_props.clean 'sed -e "s/\(.*password.*=\).*/\1/"'
 git config --global filter.rm_whiteline.clean 'sed -e "s/^[[:blank:]]+$//"'
