a08cfac8d7766034169361cdf209a9dcf40938e8
mig82 <miguelangelxfm@gmail.com>
2019-06-27 12:22:04 +0200

    Def alias to download gitignore and gitattributes

--- aliases.sh
+++ aliases.sh
@@ -8,7 +8,7 @@
 #git config --global --add alias.gui '!sh -c /usr/local/Cellar/git/2.16.3/libexec/git-core/git-gui'
 #git config --global --add alias.gui 'open -a SourceTree .'
 #git config --global --add alias.gui '/Applications/GitKraken.app/Contents/MacOS/GitKraken --path .'
-git config --global --add alias.gui 'open -a Terminal .'
+git config --global alias.gui 'open -a Terminal .'
 
 # To temporarily quiet down changes to specific files without altering your .gitignore.
 git config --global alias.mute 'update-index --assume-unchanged'
@@ -16,5 +16,9 @@ git config --global alias.unmute 'update-index --no-assume-unchanged'
 
 # To create a zip archive of your project.
 git config --global alias.zip '!git clean -fXd && git archive --format zip -9 HEAD --output $1 && ls -lh $1'
+
+# To download the .gitingore and .gitattributes into the current localtion.
+git config --global alias.visinit '!curl -L http://bit.ly/visgitignore -o .gitignore &&\
+  curl -L http://bit.ly/visgitattributes -o .gitattributes'
 
 echo "Done: Git aliases for Visualizer"
