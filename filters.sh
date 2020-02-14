81f0c89f18784d57da6cd27e949dcd9dbcd12090
Miguelángel Fernández <miguelangelxfm@gmail.com>
2020-02-14 12:00:37 +0100

    Don't track which local collection a component belongs to
    Developers may add/import the same component into different collections locally, so this would cause merge conflicts.

--- filters.sh
+++ filters.sh
@@ -23,7 +23,7 @@ git config --global filter.rm_whiteline.clean 'sed -e "s/^[[:blank:]]+$//"'
 git config --global filter.kony_projectProperties.clean "jq --indent 4 --sort-keys -j 'del(.nativeContainerProperties.isModified)'"
 
 # Prettify and sort all views -i.e. forms, templates, reusable components and pop-ups and ignore WYSIWYG metadata.
-git config --global filter.kony_views.clean "jq --indent 4 -j 'del(.device, .prevDevice, .platform, .prevPlatform, .shellCSS, .context.platform, .currentLocale, .osPlatform, .osVersion)'"
+git config --global filter.kony_views.clean "jq --indent 4 -j 'del(.device, .prevDevice, .platform, .prevPlatform, .shellCSS, .context.platform, .currentLocale, .osPlatform, .osVersion, .globalUserWidgetRefs)'"
 
 # Prettify and sort generic JSON files such as mobileFabricServiceConfigMap.json and objectServicesConfigMap.json
 git config --global filter.pretty_json.clean "jq --indent 4 --sort-keys ."
